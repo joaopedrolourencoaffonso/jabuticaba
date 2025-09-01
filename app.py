45680bc29b0ad425e131e87dba29bc043e7f719f
Joao Pedro <joaoaffonso@id.uff.br>
2025-09-01 19:58:46 -0300

    Servidor flask básico

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html');
+
+@app.route('/abrir-arquivo')
+def abrirArquivo():
+    file = open("temp.txt","r").read();
+    
+    return file;
+
+if __name__ == '__main__':
+    app.run(debug=True)
