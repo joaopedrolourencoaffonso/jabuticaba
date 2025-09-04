bdb6b3369dccadbec23790ffef7550e67fc33084
Joao Pedro <joaoaffonso@id.uff.br>
2025-09-04 19:38:34 -0300

    Adicionando endpoint para realizar chamadas para o chatGPT

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
-from functions import *
+from jabuticaba_functions import *
 
 app = Flask(__name__)
 
+@app.route('/main')
+@app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html');
@@ -17,6 +19,14 @@ def abrirArquivo():
 @app.route('/formularioSimples')
 def formularioSimples():
     return render_template('formularioSimples.html');
+
+@app.route('/chamandoChatGPT', methods=['POST'])
+def chamandoChatGPT():
+    curriculos = request.form.get('curriculos', '')
+
+    print(curriculos);
+    
+    return "ok",200;
 
 if __name__ == '__main__':
     app.run(debug=True)
